9ed315db5cf1503aac95761e428dc505ba1347a7
unknown <visicev@gmail.com>
2018-08-12 15:43:31 +0200

    Update

---
+++ SortingHacks/SortingHacks/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingHacks
+{
+    public class Person : IComparable
+    {
+        string first_name;
+        string last_name;
+        long id;
+
+        public Person()
+        {
+            this.first_name = "NoName";
+            this.last_name = "NoName";
+            this.id = 0;
+        }
+
+        public Person(string first_name,string last_name,long id)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.id = id;
+        }
+
+        public string First_name { get => first_name; set => first_name = value; }
+
+        public string Last_name { get => last_name; set => last_name = value; }
+
+        public long Id { get => id; set => id = value; }
+
+        public int CompareTo(object obj)
+        {
+            Person p = obj as Person;
+            return p.Id.CompareTo(this.Id);
+        }
+
+        public override string ToString()
+        {
+            return First_name + " " + Last_name + " ID: " + Id;
+        }
+    }
+}
---
+++ SortingHacks/SortingHacks/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingHacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Vuk", "Isic", 2611);
+            Person p2 = new Person("Vuk", "Isic", 2612);
+            Person p3 = new Person("Vuk", "Isic", 2613);
+            Person p4 = new Person("Vuk", "Isic", 2614);
+            Person p5 = new Person("Vuk", "Isic", 2615);
+
+            List<Person> list = new List<Person>();
+            List<Person> list2 = new List<Person>();
+            list.Add(p1);
+            list.Add(p3);
+            list.Add(p2);
+            list.Add(p5);
+            list.Add(p4);
+
+            list2.Add(p1);
+            list2.Add(p3);
+            list2.Add(p2);
+            list2.Add(p5);
+            list2.Add(p4);
+
+            Console.WriteLine("UnSorted List:");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            // IComparable Method!
+            list.Sort();
+
+            Console.WriteLine("Sorted List:");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("UnSorted List2:");
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Lambda Method!
+            list2.Sort((emp1, emp2) => emp2.Id.CompareTo(emp1.Id));
+
+            Console.WriteLine("Sorted List2:");
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
