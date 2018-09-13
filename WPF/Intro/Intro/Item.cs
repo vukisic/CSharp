6dea13a513cc8e0e6d0b7e44f3e07e3f9f9acb37
vukisic <visicev@gmail.com>
2018-09-13 19:06:48 +0200

    Update

---
+++ WPF/Intro/Intro/Item.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro
+{
+    public class Item
+    {
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Revision { get; set; }
+        public int PartNumber { get; set; }
+        public string Material { get; set; }
+        public double Length { get; set; }
+        public int Mass { get; set; }
+        public string Finish { get; set; }
+        public string PruchaseInfo { get; set; }
+        public string SupplierName { get; set; }
+        public int SupplierCode { get; set; }
+        public string Note { get; set; }
+
+        public List<string> WorkCentres { get; set; }
+
+        public Item()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            string output="";
+            output += "Description: " + Description + Environment.NewLine;
+            output += "Status: " + Status + Environment.NewLine;
+            output += "Revision: " + Revision + Environment.NewLine;
+            output += "PartNumber: " + PartNumber.ToString() + Environment.NewLine;
+            output += "Material: " + Material + Environment.NewLine;
+            output += "WorkCentres: " + Environment.NewLine;
+            foreach (var item in WorkCentres)
+            {
+                output += "\t" + item + Environment.NewLine;
+            }
+            output += "Length: " + Length.ToString() + Environment.NewLine;
+            output += "Mass: " + Mass.ToString() + Environment.NewLine;
+            output += "Finish: " + Finish + Environment.NewLine;
+            output += "PurchaseInfo: " + PruchaseInfo + Environment.NewLine;
+            output += "SupplierName: " + SupplierName + Environment.NewLine;
+            output += "SupplierCode: " + SupplierCode.ToString() + Environment.NewLine;
+            output += "Note: " + Note + Environment.NewLine;
+
+            return output;
+                           
+        }
+
+    }
+}
---
+++ WPF/Intro/Intro/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace Intro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<string> workList = new List<string>();
+        static string material;
+        static string finish;
+        static string purchase;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Reset()
+        {
+            this.DescriptionTextBox.Text = "";
+            this.LengthTextBox.Text = "";
+            this.MassTextBox.Text = "";
+            this.NoteTextBox.Text = "";
+            this.PartNumTextBox.Text = "";
+            this.RevisionTextBox.Text = "";
+            this.SCodeTextBox.Text = "";
+            this.SNameTextBox.Text = "";
+            this.StatusTextBox.Text = "";
+
+            this.AssemplyCB.IsChecked = this.DrillCB.IsChecked = this.FoldCB.IsChecked = this.LaserCB.IsChecked
+            = this.LatheCB.IsChecked = this.PlasmaCB.IsChecked = this.PurchesCB.IsChecked = this.RollCB.IsChecked
+            = this.SawCB.IsChecked = this.WeldCB.IsChecked = false;
+
+            this.RowMaterialComboBox.SelectedIndex = 0;
+            this.FinishComboBox.SelectedIndex = 0;
+            this.PurchesComboBox.SelectedIndex = 0;
+
+            workList.Clear();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Item item = new Item
+            {
+                Description = DescriptionTextBox.Text,
+                Length = Double.Parse(LengthTextBox.Text),
+                Mass = Int32.Parse(MassTextBox.Text),
+                Note = NoteTextBox.Text,
+                PartNumber = Int32.Parse(PartNumTextBox.Text),
+                Revision = RevisionTextBox.Text,
+                SupplierCode = Int32.Parse(SCodeTextBox.Text),
+                SupplierName = SNameTextBox.Text,
+                Status=StatusTextBox.Text,
+                WorkCentres=workList,
+                Material=material,
+                Finish=finish,
+                PruchaseInfo=purchase
+            };
+
+            using (StreamWriter sw=new StreamWriter("../../../output.txt"))
+            {
+                sw.WriteAsync(item.ToString());
+            }
+
+            string JSONdata = JsonConvert.SerializeObject(item,Formatting.Indented);
+            File.WriteAllText("../../../output.json", JSONdata);
+
+            Reset();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            Reset();
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.AssemplyCB.IsChecked = this.DrillCB.IsChecked = this.FoldCB.IsChecked = this.LaserCB.IsChecked
+            = this.LatheCB.IsChecked = this.PlasmaCB.IsChecked = this.PurchesCB.IsChecked = this.RollCB.IsChecked
+            = this.SawCB.IsChecked = this.WeldCB.IsChecked = false;
+
+            this.RowMaterialComboBox.SelectedIndex = 0;
+            this.FinishComboBox.SelectedIndex = 0;
+            this.PurchesComboBox.SelectedIndex = 0;
+
+            workList.Clear();
+        }
+
+        private void CB_Checked(object sender, RoutedEventArgs e)
+        {
+            workList.Add(((CheckBox)sender).Content.ToString());
+        }
+
+        private void CB_Unchecked(object sender, RoutedEventArgs e)
+        {
+            string item = ((CheckBox)sender).Content.ToString();
+            if(workList.Contains(item))
+            {
+                workList.Remove(item);
+            }
+        }
+
+        private void RowMaterilaComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            material = ((ComboBoxItem)((ComboBox)sender).SelectedValue).Content.ToString();
+        }
+
+        private void FinishComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            finish = ((ComboBoxItem)((ComboBox)sender).SelectedValue).Content.ToString();
+        }
+
+        private void PurchaseComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            purchase = ((ComboBoxItem)((ComboBox)sender).SelectedValue).Content.ToString();
+        }
+    }
+}
