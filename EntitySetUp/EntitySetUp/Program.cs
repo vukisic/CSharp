4887ad3dd5890cda36768a13d313ddda5eaa1f97
vukisic <visicev@gmail.com>
2018-09-18 16:40:31 +0200

    Update

---
+++ EntitySetUp/EntitySetUp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitySetUp
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public int Price { get; set; }
+    }
+
+    public class ItemDbContex : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
